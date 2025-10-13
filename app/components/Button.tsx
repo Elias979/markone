feeb70eaecc228222fd038d1b802e8e7b9d5c540
Ilyas Yildirim <elias.yildirim97@hotmail.com>
2025-10-13 23:49:26 +0200

    MY

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,13 @@
+'use client'; // <-- Lägg till den här raden överst
+
 import styles from "./Button.module.css";
 
-export default function Button({ children, onClick }:{children: React.ReactNode; onClick?: ()=>void}) {
+export default function Button({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: () => void;
+}) {
   return <button className={styles.btn} onClick={onClick}>{children}</button>;
 }
